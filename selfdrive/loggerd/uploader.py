3eae4e79ff299e4bde0984101bf3d9c5c0c21b6b
Willem Melching <willem.melching@gmail.com>
2020-10-01 16:00:52 +0200

    testing closet changes

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -126,8 +126,7 @@ class Uploader():
         return (name, key, fn)
 
     for name, key, fn in upload_files:
-      if name in self.immediate_priority:
-        return (name, key, fn)
+      return (name, key, fn)
 
     return None
 
@@ -143,7 +142,7 @@ class Uploader():
       headers = url_resp_json['headers']
       cloudlog.debug("upload_url v1.4 %s %s", url, str(headers))
 
-      if fake_upload:
+      if fake_upload or (not fn.endswith('rlog')):
         cloudlog.debug(f"*** WARNING, THIS IS A FAKE UPLOAD TO {url} ***")
 
         class FakeResponse():
@@ -151,6 +150,9 @@ class Uploader():
             self.status_code = 200
 
         self.last_resp = FakeResponse()
+
+        # delete file to keep as many rlogs as possible
+        os.remove(fn)
       else:
         with open(fn, "rb") as f:
           data = f.read()
@@ -158,7 +160,7 @@ class Uploader():
           if key.endswith('.bz2') and not fn.endswith('.bz2'):
             data = bz2.compress(data)
 
-          self.last_resp = requests.put(url, data=data, headers=headers, timeout=10)
+          self.last_resp = requests.put(url, data=data, headers=headers, timeout=100)
     except Exception as e:
       self.last_exc = (e, traceback.format_exc())
       raise
@@ -186,7 +188,7 @@ class Uploader():
     if sz == 0:
       # tag files of 0 size as uploaded
       success = True
-    elif name in self.immediate_priority and sz > UPLOAD_QLOG_QCAM_MAX_SIZE:
+    elif name in self.immediate_priority and sz > UPLOAD_QLOG_QCAM_MAX_SIZE and False:
       cloudlog.event("uploader_too_large", key=key, fn=fn, sz=sz)
       success = True
     else:
